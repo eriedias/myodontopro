26b62d01696299e8916a61e5810b414997705fef
Erie Dias <eriedias7@gmail.com>
2026-06-26 12:14:15 -0300

    finalizada Aula 91... antes de começar Stripe

---
+++ app/(panel)/dashboard/plans/_components/grid-plans.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { subscriptionPlans } from "@/utils/plans/index";
+
+export function GridPlans() {
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-5">
+      {subscriptionPlans.map((plan, index) => (
+        <Card
+          key={plan.id}
+          className={`flex flex-col w-full mx-auto ${index === 1 && "border-emerald-500"}`}
+        >
+          {index === 1 && (
+            <div className="bg-emerald-500 w-full py-3 text-center rounded-t-xl">
+              <p className="font-semibold text-white">PROMOÇÃO EXCLUSIVA</p>
+            </div>
+          )}
+
+          <CardHeader>
+            <CardTitle className="text-xl md:text-2xl">{plan.name}</CardTitle>
+            <CardDescription>{plan.description}</CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <ul>
+              {plan.features.map((feature, index) => (
+                <li key={index} className="text-sm md:text-base">
+                  {feature}
+                </li>
+              ))}
+            </ul>
+
+            <div className="mt-4">
+              <p className="text-gray-600 line-through">{plan.oldPrice}</p>
+              <p className="text-black text-2xl font-bold">{plan.price}</p>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full bg-black">Ativar assinatura</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </section>
+  );
+}
--- app/(panel)/dashboard/plans/page.tsx
+++ app/(panel)/dashboard/plans/page.tsx
@@ -1,7 +1,24 @@
-export default function Plans() {
-    return (
-        <section>
-            <h1>Página de Planos</h1>
-        </section>
-    )
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
+import { GridPlans } from "./_components/grid-plans";
+import { getSubscription } from "@/utils/get-subscription";
+
+export default async function Plans() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const subscritpion = await getSubscription({ userId: session?.user?.id! });
+
+  return (
+    <div>
+      {subscritpion?.status !== "active" && <GridPlans />}
+
+      {subscritpion?.status === "active" && (
+        <h1>Você tem uma assinatura ativa</h1>
+      )}
+    </div>
+  );
 }
