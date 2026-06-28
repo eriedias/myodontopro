6a93e33e9bb50a6bd65d60fd74cdfb739f283c72
Erie Dias <eriedias7@gmail.com>
2026-06-27 23:45:06 -0300

    finalizada Seção 7

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
